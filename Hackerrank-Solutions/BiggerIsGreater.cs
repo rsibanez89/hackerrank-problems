747303992018e7a858e88d84bfc736b2726d9416
Rodrigo <rsibanez89@gmail.com>
2016-10-20 02:20:18 +1100

    Uploading problem BiggerIsGreater and its solution. Solves the problem of the Next Lexicographical Permutation

--- Hackerrank-Solutions/BiggerIsGreater.cs
+++ Hackerrank-Solutions/BiggerIsGreater.cs
@@ -11,46 +11,34 @@ namespace Hackerrank_Solutions
         private static string[] inputs = new string[] { "6", "ab", "bb", "hefg", "dhck", "dkhc", "zzzayybbaa" };
         private static string[] output = new string[] { "ba", "no answer", "hegf", "dhkc", "hcdk", "zzzbaaabyy" };
 
-        public int descendigBinarySeach(char[] word, char toInsert, int start, int end)
+        public int getIndexToSwap(char[] word, char toInsert, int start, int end)
         {
             while (start <= end)
             {
                 int middle = (start + end) / 2;
                 if (word[middle] > toInsert)
                     start = middle + 1;
-                else if (word[middle] < toInsert)
+                else if (word[middle] <= toInsert) // if is equal I want the left one
                     end = middle - 1;
-                else
-                    return middle; // found
             }
-            return -(start + 1);  // not found
+            return start - 1;  // not found
         }
 
+        // This algorithm follows this logic
+        // https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
         public bool nextLexicographicalPermutationBinarySeach(char[] word)
         {
             int index = word.Length - 1;
-            bool found = false;
             char swap;
 
             // 1. find the pivot to swap
-            while (index > 0 && !found)
-            {
-                if (word[index - 1] < word[index]) // I have a swap 
-                    found = true;
-                else
-                    index--;
-            }
+            while (index > 0 && (word[index - 1] >= word[index]))
+                index--;
 
-            if (!found) // there is no permutation
+            if (index == 0) // there is no permutation
                 return false;
 
-            int indexToSwap = descendigBinarySeach(word, word[index - 1], index, word.Length-1);
-            if (indexToSwap < 0)
-                indexToSwap = ~indexToSwap - 1; // inverse order
-            else
-                if (indexToSwap > 0)
-                    indexToSwap--;
-
+            int indexToSwap = getIndexToSwap(word, word[index - 1], index, word.Length - 1);      
 
             // 2. swap
             swap = word[indexToSwap];
@@ -58,42 +46,7 @@ namespace Hackerrank_Solutions
             word[index - 1] = swap;
 
             // 4. reverse the sufix
-            int j = word.Length - 1;
-            while (index < j)
-            {
-                swap = word[index];
-                word[index] = word[j];
-                word[j] = swap;
-                index++;
-                j--;
-            }
-
-            return true;
-        }
-
-        static public bool nextPermutation(char[] word)
-        {
-
-            int n = word.Length;
-            int k = -1;
-
-            for (int i = 1; i < n; i++)
-                if (word[i - 1] < word[i])
-                    k = i - 1;
-
-            if (k == -1)
-                return false;
-
-            int l = k + 1;
-            for (int i = l; i < n; i++)
-                if (word[k] < word[i])
-                    l = i;
-
-            char t = word[k];
-            word[k] = word[l];
-            word[l] = t;
-
-            Array.Reverse(word, k + 1, word.Length - (k + 1));
+            Array.Reverse(word, index, word.Length - index);
 
             return true;
         }
@@ -107,15 +60,16 @@ namespace Hackerrank_Solutions
                 char[] word = inputs[nLine++].ToArray();
 
                 if (word.Length > 1 && nextLexicographicalPermutationBinarySeach(word))
-                    for (int i = 0; i < word.Length; i++)
-                        Console.Write(word[i]);
+                    //for (int i = 0; i < word.Length; i++)    <-- THIS WAY OF PRINTING GIVES YOU TIME OUT
+                    //    Console.Write(word[i]);                
+                    Console.WriteLine(new string(word));
                 else
                     Console.Write("no answer");
                 Console.WriteLine();
             }
         }
 
-        // This algorithm follows that logic
+        // This algorithm follows this logic
         // https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
         public bool nextLexicographicalPermutation(char[] word)
         {
@@ -156,6 +110,7 @@ namespace Hackerrank_Solutions
             return true;
         }
 
+        //Terminated due to timeout
         public void Solution2()
         {
             int nLine = 0;
@@ -166,7 +121,7 @@ namespace Hackerrank_Solutions
 
                 if (word.Length > 1 && nextLexicographicalPermutation(word))
                     for (int i = 0; i < word.Length; i++)
-                        Console.Write(word[i]);
+                        Console.Write(word[i]); // <---- THIS IS THE TIME OUT!!!!
                 else
                     Console.Write("no answer");
                 Console.WriteLine();
